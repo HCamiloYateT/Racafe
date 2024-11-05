3f44142b5e5312b56a283690710cbfa791295f8f
HCamiloYateT <hcyate@racafe.com>
2024-11-05 14:30:38 -0500

    Adicion de Funcion de Sankey

--- R/Outputs.R
+++ R/Outputs.R
@@ -50,3 +50,149 @@ CajaIco <- function(texto, icono, col_fondo = "#FDFEFE", alto = 120, col_letra =
          )
   )
 }
+
+#' Genera un diagrama de Sankey basado en un conjunto de datos
+#'
+#' La función `ImprimeSankey` crea un diagrama de Sankey interactivo a partir de un conjunto de datos, utilizando variables y colores definidos por el usuario.
+#'
+#' @param data Data frame que contiene los datos a graficar.
+#' @param vars Vector de nombres de variables que serán utilizadas en el diagrama de Sankey.
+#' @param fun Función de resumen a aplicar (por ejemplo, "n" para el conteo de filas).
+#' @param var Nombre de la variable que se utilizará en la función de resumen (solo necesario si `fun` no es "n").
+#' @param colores Vector de colores que corresponde a cada una de las variables especificadas en `vars`.
+#'
+#' @return Una lista con dos elementos: el gráfico de Sankey (`plot`) y los data frames de nodos y arcos.
+#' @examples
+#' # Ejemplo de uso
+#' ImprimeSankey(data = df, vars = c("variable1", "variable2"), fun = "mean", var = "variable3", colores = c("blue", "green"))
+ImprimeSankey <- function(data, vars, fun, var = NULL, colores) {
+
+  # Verifica que el número de colores coincida con el número de variables
+  if (length(vars) != length(colores)) {
+    stop("El número de colores debe coincidir con el número de variables.")
+  }
+
+  # Crear una tabla de frecuencias para los valores únicos en las variables especificadas
+  tb <- table(unlist(sapply(vars, function(x) unique(data[[x]]))) %>% as.character())
+  tb <- tb[tb > 1] %>% unlist() %>% names()
+
+  # Resumir los datos utilizando la función especificada
+  if (fun == "n") {
+    # Contar filas si `fun` es "n"
+    aux1 <- data %>%
+      group_by(Origen = "TOTAL", across(all_of(vars))) %>%
+      summarise(Tot = n(), .groups = 'drop') %>%
+      mutate(across(all_of(vars), ~ ifelse(. %in% c(tb, "", NA),
+                                           paste0(cur_column(), .), .)))
+  } else {
+    # Aplicar la función especificada a `var` si `fun` no es "n"
+    aux1 <- data %>%
+      group_by(Origen = "TOTAL", across(all_of(vars))) %>%
+      summarise(Tot = !!parse_expr(paste0(fun, "(",
+                                          var, ", na.rm = TRUE)")), .groups = 'drop') %>%
+      mutate(across(all_of(vars), ~ ifelse(. %in% c(tb, "", NA),
+                                           paste0(cur_column(), .), .)))
+  }
+
+  # Preparar etiquetas de nodos y colores
+  vec <- c("Origen", vars)
+  nds <- c("TOTAL", unlist(sapply(vars, function(x) unique(aux1[[x]]))) %>% as.character())
+
+  # Generar paletas de colores personalizadas para cada variable en `vars`
+  aux_col <- unlist(sapply(vars, function(x) {
+    i = {parent.frame()$i[]}
+    color_base <- colores[i]
+    n_colores <- length(unique(aux1[[x]]))
+    paleta <- colorRampPalette(c(lighten(color_base, 0.5), color_base, darken(color_base, 0.5)))(n_colores)
+  })) %>% as.character()
+
+  # Crear descripciones de texto para cada nodo
+  aux_txt <- unlist(sapply(vec, function(x) {
+    tot <- sum(aux1$Tot)
+    aux1 %>%
+      group_by(across(all_of(x))) %>%
+      summarise(Tot = sum(Tot), Pct = Tot / tot, .groups = 'drop') %>%
+      rowwise() %>%
+      mutate(txt = paste0("<b>Clientes: </b>", comma(Tot, accuracy = 1),
+                          "<br><b>Pct. del Total: </b>", percent(Pct, accuracy = 0.01))) %>%
+      pull(txt)
+  })) %>% as.character()
+
+  # Crear etiquetas para cada variable en los nodos
+  aux_var <- unlist(sapply(vec, function(x) {
+    tot <- sum(aux1$Tot)
+    aux1 %>%
+      group_by(across(all_of(x))) %>%
+      summarise(Tot = sum(Tot), Pct = Tot / tot, .groups = 'drop') %>%
+      mutate(Var = x) %>%
+      pull(Var)
+  })) %>% as.character()
+
+  # Crear el data frame de nodos con etiquetas, colores y descripciones
+  nodos <- data.frame(
+    label = nds,
+    label2 = Reduce(function(x, pattern) gsub(pattern, "", x), vars, nds),
+    indices = seq_along(nds) - 1,
+    colores = c("black", aux_col),
+    AuxTexto = aux_txt,
+    Var = aux_var
+  ) %>%
+    mutate(texto = paste0("<b>", label2, "</b><br>", AuxTexto))
+
+  # Crear el data frame de enlaces (links) entre nodos
+  n <- length(vec) - 1
+  aux_lista <- lapply(1:n, function(i) c(vec[i], vec[i + 1]))
+  links <- do.call("bind_rows", lapply(aux_lista, function(x) {
+    aux1 %>%
+      group_by(across(all_of(x))) %>%
+      summarise(Tot = sum(Tot), .groups = 'drop') %>%
+      left_join(nodos %>% select(label, indices, VarSource = Var) %>% rename(source = indices), by = setNames("label", x[1]))  %>%
+      left_join(nodos %>% select(label, indices, VarTarget = Var) %>% rename(target = indices), by = setNames("label", x[2])) %>%
+      select(source, target, value = Tot, VarSource, VarTarget)
+  }))
+
+  # Calcular valores totales para los enlaces
+  total_values <- aggregate(value ~ source, data = links, sum)
+
+  links <- links %>%
+    left_join(total_values, by = "source", suffix = c("", "_total")) %>%
+    left_join(nodos %>% select(source = indices, Origen = label2), by = "source") %>%
+    left_join(nodos %>% select(target = indices, Destino = label2), by = "target") %>%
+    mutate(
+      PctTot = value / sum(aux1$Tot),
+      PctSource = value / value_total,
+      texto = paste0("<b>Origen: </b>", Origen,
+                     "<br><b>Destino: </b>", Destino,
+                     "<br><b>Clientes: </b>", comma(value, accuracy = 1),
+                     "<br><b>Pct. del Total: </b>", percent(PctTot, accuracy = 0.01),
+                     "<br><b>Pct. del Origen: </b>", percent(PctSource, accuracy = 0.01)
+      )
+    )
+
+  # Crear el diagrama de Sankey con `plotly`
+  sankey <- plot_ly(
+    type = "sankey",
+    arrangement= "fixed",
+    orientation = "h",
+    node = list(
+      pad = 15,
+      thickness = 20,
+      line = list(color = "black", width = 0.5),
+      label = nodos$label2,
+      color = nodos$color,
+      hovertemplate = paste0("%{customdata}%", "<extra></extra>"),
+      customdata = nodos$texto
+    ),
+    link = list(
+      hovertemplate = paste0("%{customdata}%", "<extra></extra>"),
+      customdata = links$texto,
+      source = links$source,
+      target = links$target,
+      value = links$value,
+      color = "rgba(0,0,0,0.3)")
+  ) %>%
+    layout(clickmode = "event+select") %>%
+    event_register("plotly_click")
+
+  return(list(plot = sankey, nodos = nodos, arcos = links))
+}
